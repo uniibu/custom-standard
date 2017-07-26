94e09559dd45a5242f92ca2318fd3cd97bbff00b
uniibu <unibtc@gmail.com>
2017-07-26 15:33:22 +0000

    fix package.json location

--- options.js
+++ options.js
@@ -1,13 +1,13 @@
 var path = require('path');
 var pkg = require('./package.json');
-
+var mainpkg = require(path.resolve(pkg['_where'], 'package.json'));
 module.exports = {
   // cmd, homepage, bugs all pulled from package.json
   cmd: 'customstandard',
   version: pkg.version,
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  extra_rules: pkg.customstandard ? pkg.customstandard.rules : null,
+  extra_rules: mainpkg.customstandard ? mainpkg.customstandard.rules : null,
   tagline: 'Custom Standard For All!',
   eslint: require('eslint'),
   eslintConfig: {
